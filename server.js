325b91ad148a66f5c5c693ec7f7c844bcd4959e1
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-26 20:21:19 -0200

    Add help.html

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello Express!</h1>');
 });
@@ -17,4 +19,6 @@ app.get('/bad', (req, res) => {
 });
 
 // Binds the app to the port 3000
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is listening the port 3000');
+});
